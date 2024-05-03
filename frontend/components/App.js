a9588cfd1d79a1804afeb64b84c6c84e92561b64
KeithLamb <keithianlamb@gmail.com>
2024-05-03 14:36:56 -0500

    Use react js framework to create stars wars web app first turn in to codegrade

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,17 +2,26 @@ import React from 'react'
 import axios from 'axios'
 import Character from './Character'
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+// const urlPlanets = 'http://localhost:9009/api/planets'
+// const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const response = await axios.get('http://localhost:9009/api/people');
+      setCharacters(response.data);
+    };
+
+    fetchCharacters();
+  }, []);
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map((character) => (
+        <Character key={character.id} {...character} />
+      ))}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
+function Character({ name, homeworld, gender, height, eye_color }) {
+  const [showHomeworld, setShowHomeworld] = useState(false);
+
+  const toggleHomeworld = () => setShowHomeworld(!showHomeworld);
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div onClick={toggleHomeworld}>
+      <h3>{name}</h3>
+      {showHomeworld && <p>Homeworld: {homeworld}</p>}
+      <p>Gender: {gender}</p>
+      <p>Height: {height}</p>
+      <p>Eye Color: {eye_color}</p>
     </div>
   )
 }
